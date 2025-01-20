40d6c007ba76ec5db1c0dc91b689536c9607483c
piyush rokaya <piyushrokaya600@gmail.com>
2025-01-20 21:32:03 +0545

    deleted file

---
+++ online-js/javascript.js
@@ -0,0 +1,27 @@
+// console.log("one")
+// setTimeout(() => {
+//     console.log('hello')
+// }, 2000);
+// console.log("two")
+
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// function calculator(a,b,sumcallback){
+//     sumcallback(a,b);//this will pass a and b in sum function
+// }
+//calculator(1,2,sum);//sum is already defined function 
+//only use sum as parameter
+
+
+// function calculator(a,b,sumcallback){
+//     sumcallback(a,b);
+
+// }
+// calculator(1,2,(a,b)=>{
+//     console.log(a+b);
+// })
+
+//callback hell
+
